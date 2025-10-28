bb4426eafe3d7535c661880b92f216756611e127
d.balyaev <d.balyaev@npo-echelon.ru>
2025-10-28 10:17:29 +0300

    chore(init): init repo

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,38 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	models "github.com/ibeloyar/metrics/internal/model"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /update/{type}/{name}/{value}", func(w http.ResponseWriter, r *http.Request) {
+		t := r.PathValue("type")
+		n := r.PathValue("name")
+		v := r.PathValue("value")
+
+		fmt.Printf("%s %s %s\n", t, n, v)
+
+		if t != models.Gauge && t != models.Counter {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		_, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		panic(err)
+	}
+}
