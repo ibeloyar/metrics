056d73a2c637186282433249f895b601fd123bc6
ibeloyar <beloyar.dev@outlook.com>
2025-12-12 07:32:45 +0300

    fix(review): agent retry

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/go-chi/chi/v5 v5.2.3 // indirect
 	github.com/golang-migrate/migrate/v4 v4.19.1 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.8 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
--- internal/agent/service/service.go
+++ internal/agent/service/service.go
@@ -8,10 +8,15 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/hashicorp/go-retryablehttp"
 )
 
 const (
-	maxSendAttempts = 3
+	maxSendAttempts     = 3
+	firstRetryDuration  = 1 * time.Second
+	secondRetryDuration = 3 * time.Second
+	lastRetryDuration   = 5 * time.Second
 )
 
 type Service struct {
@@ -27,88 +32,63 @@ type SendMetricBody struct {
 }
 
 func NewService(addr string) *Service {
+	client := retryablehttp.NewClient()
+	client.RetryMax = maxSendAttempts
+	client.RetryWaitMin = firstRetryDuration
+	client.RetryWaitMax = lastRetryDuration
+	client.Backoff = customBackoff
+	standardClient := client.StandardClient()
+
 	return &Service{
-		client: &http.Client{},
+		client: standardClient,
 		addr:   addr,
 	}
 }
 
 func (s *Service) SendMetrics(metrics []SendMetricBody) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 6*time.Second)
+	defer cancel()
+
 	bodyBytes, err := json.Marshal(metrics)
 	if err != nil {
 		return err
 	}
 
-	return s.doSendWithRetry(bodyBytes)
-}
-
-func (s *Service) doSendWithRetry(body []byte) error {
-	response, err := s.doSendGzip(body)
-	if err != nil {
-		var lastErr error
-
-		lastErr = err
-
-		for attempt := 0; attempt < maxSendAttempts; attempt++ {
-			delay := getDelayFromAttempt(attempt)
-			time.Sleep(delay)
-
-			response, err = s.doSendGzip(body)
-			response.Body.Close()
-
-			if err == nil {
-				return nil
-			}
-
-			lastErr = err
-		}
-
-		return lastErr
-	}
-	response.Body.Close()
-
-	return nil
-}
-
-func (s *Service) doSendGzip(body []byte) (*http.Response, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
 	var buf bytes.Buffer
 	gw := gzip.NewWriter(&buf)
 
-	_, err := gw.Write(body)
-	if err != nil {
-		return nil, err
+	if _, err = gw.Write(bodyBytes); err != nil {
+		return err
 	}
 
-	err = gw.Close()
-	if err != nil {
-		return nil, err
+	if err = gw.Close(); err != nil {
+		return err
 	}
 
 	request, err := http.NewRequestWithContext(ctx, http.MethodPost, fmt.Sprintf("http://%s/updates/", s.addr), bytes.NewReader(buf.Bytes()))
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	request.Header.Set("Content-Type", "application/json")
 	request.Header.Set("Content-Encoding", "gzip")
 
-	response, err := s.client.Do(request)
+	if _, err = s.client.Do(request); err != nil {
+		return err
+	}
 
-	return response, err
+	return nil
 }
 
-func getDelayFromAttempt(attempt int) time.Duration {
-	switch attempt {
+func customBackoff(min, max time.Duration, attemptNum int, _ *http.Response) time.Duration {
+	switch attemptNum {
 	case 0:
-		return 1 * time.Second
+		return min
 	case 1:
-		return 3 * time.Second
+		return secondRetryDuration
 	case 2:
 		fallthrough
 	default:
-		return 5 * time.Second
+		return max
 	}
 }
