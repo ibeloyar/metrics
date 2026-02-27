6934141ed0f881dd43c5625765aba38a9bdb395f
ibeloyar <beloyar.dev@outlook.com>
2026-02-27 12:27:39 +0300

    fix: reset gen pkg

--- cmd/reset/mock/reset_struct.go
+++ cmd/reset/mock/reset_struct.go
@@ -1,1 +1,35 @@
 package mock
+
+type ChildWithReset struct {
+	Field string
+}
+
+func (cr *ChildWithReset) Reset() {
+	cr.Field = ""
+}
+
+// generate:reset
+type ResetableStruct struct {
+	i              int
+	str            string
+	strP           *string
+	intP           *int
+	boolP          *bool
+	s              []int
+	m              map[string]string
+	child          *ResetableStruct
+	childWithReset *ChildWithReset
+}
+
+//// generate:reset
+//type ResetableStruct2 struct {
+//	i              int
+//	str            string
+//	strP           *string
+//	intP           *int
+//	boolP          *bool
+//	s              []int
+//	m              map[string]string
+//	child          *ResetableStruct
+//	childWithReset *ChildWithReset
+//}
